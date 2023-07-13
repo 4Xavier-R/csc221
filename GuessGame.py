2b4673bb82cdb1847a77f2a2a4ebb554a3c7dd35
Xavier Rodriguez <xr63645@email.vccs.edu>
2023-07-13 09:18:56 -0400

    spaces

--- GuessGame.py
+++ GuessGame.py
@@ -2,27 +2,27 @@ from random import randint
 from gasp.utils import read_yesorno
 while True:
     
-    question = "what's your guess?"
-    rquestion = "What would you like the range to be. 1 to ?"
+    question = "what's your guess?\n"
+    rquestion = "What would you like the range to be. 1 to ?\n"
     r = (int(input(rquestion)))
     number = randint(1, r)
-    print("I've choses a number from 1 to " + str(r) + ", try to guess it")
+    print("I've thought of a number between 1 and " + str(r) + ", try to guess it.")
     guess = 0
     guesses = 0
     while True:
         guess = (int(input(question)))
         if guess < number:
-            print("The number is higher than that")
+            print("The number is higher than that.\n")
             guesses = guesses +1
         if guess > number:
-            print("The number is lower than that")
+            print("The number is lower than that.\n")
             guesses = guesses +1
         if guess == number:
             print("That's Correct!")
             guesses = guesses +1
             print("Your total number of guesses was " + str(guesses))
             break
-    if read_yesorno("Would you like to play again?"):
+    if read_yesorno("Would you like to play again?\n"):
         print("Alright")
     else:
         break
