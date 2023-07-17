d5f04da0c14085e56873c2519b77564058432458
Xavier Rodriguez <xr63645@email.vccs.edu>
2023-07-17 12:57:00 -0400

    bot code

--- robots/robots.py
+++ robots/robots.py
@@ -2,25 +2,38 @@ from gasp import *
 from random import randint
 player_x = randint(0, 63) 
 player_y = randint(0, 47)
- 
+bot_x = 1
+bot_y = 1 
 def place_robots():
     print('bobot')
+    b = Box((10 * bot_x +5 , 10 * bot_y + 5), 5 , color =color.RED)
+def move_robot():
+    print('bot move')
+    if bot_y > player_y:
+        bot_y -= 1
+    if bot_y < player_y:
+        bot_y +=1
+    if bot_x > player_x:
+        bot_x -= 1
+    if bot_x < player_x:
+        bot_x += 1
 def place_player():
     global c
-    c = Circle((10 * player_x + 5 , 10 * player_y + 5), 5, filled = True)
+    c = Circle((10 * player_x + 5 , 10 * player_y + 5), 5, filled = True, color = color.PURPLE)
+
 def move_player():
     global player_x, player_y, c
     print('move')
     key = update_when('key_pressed')
-    if key == 'Up' and player_y <480:
-        player_y += 20
-    if key == 'Down' and player_y >0:
-        player_y -=20
-    if key == 'Left' and player_x >0:
-        player_x -= 20
-    if key == 'Right' and player_x <630:
-        player_x += 20
-    move_to(c, (player_x, player_y))
+    if key == 'Up' and player_y <47:
+        player_y += 1
+    if key == 'Down' and player_y >1:
+        player_y -=1
+    if key == 'Left' and player_x >1:
+        player_x -= 1
+    if key == 'Right' and player_x <63:
+        player_x += 1
+    move_to(c, (10 * player_x, 10 * player_y))
 
     
 begin_graphics()
