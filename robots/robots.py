73f21ea6487bab2bbcc70e6b9772a2e9525761ec
Xavier Rodriguez <xr63645@email.vccs.edu>
2023-07-19 11:21:34 -0400

    classes

--- robots/robots.py
+++ robots/robots.py
@@ -1,33 +1,47 @@
 from gasp import * 
 from random import randint
-player_x = 1
-player_y = 1
-bot_x = randint(0, 63)
-bot_y = randint(0, 47)
+
+
+class Robot:
+    pass
+
+
+class Player:
+    pass
+
+
 def place_robot():
-    global b
-    b = Circle((10 * bot_x +5 , 10 * bot_y + 5), 5 , color =color.RED)
+    global b, bot
+    bot = Robot()
+    bot.x = randint(0, 63)
+    bot.y = randint(0, 47)
+    b = Circle((10 * bot.x +5 , 10 * bot.y + 5), 5 , color =color.RED)
+
+
 def move_robot():
-    global bot_x, bot_y, b
+    global bot, player, b
     print('bot move')
-    if bot_y > player_y:
-        bot_y -= 1
-    if bot_y < player_y:
-        bot_y +=1
-    if bot_x > player_x:
-        bot_x -= 1
-    if bot_x < player_x:
-        bot_x += 1
-    move_to(b, (10 * bot_x, 10 * bot_y))
-    
+    if bot.y > player.y:
+        bot.y -= 1
+    if bot.y < player.y:
+        bot.y +=1
+    if bot.x > player.x:
+        bot.x -= 1
+    if bot.x < player.x:
+        bot.x += 1
+    move_to(b, (10 * bot.x, 10 * bot.y))
+
+
 def place_player():
-    global c, player_x, player_y
-    player_x = randint(0, 63) 
-    player_y = randint(0, 47)
-    c = Circle((10 * player_x + 5 , 10 * player_y + 5), 5, filled = True, color = color.PURPLE)
+    global c, player
+    player = Player()
+    player.x = randint(0, 63) 
+    player.y = randint(0, 47)
+    c = Circle((10 * player.x + 5 , 10 * player.y + 5), 5, filled = True, color = color.PURPLE)
+
 
 def move_player():
-    global player_x, player_y, c
+    global player, c
     print('move')
     key = update_when('key_pressed')
     while key == 'space':
@@ -35,34 +49,41 @@ def move_player():
         remove_from_screen(c)
         safe_player()
         key = update_when('key_pressed')
-    if key == 'Up' and player_y <47:
-        player_y += 1
-    if key == 'Down' and player_y >1:
-        player_y -=1
-    if key == 'Left' and player_x >1:
-        player_x -= 1
-    if key == 'Right' and player_x <63:
-        player_x += 1
+    if key == 'Up' and player.y <47:
+        player.y += 1
+    if key == 'Down' and player.y >1:
+        player.y -=1
+    if key == 'Left' and player.x >1:
+        player.x -= 1
+    if key == 'Right' and player.x <63:
+        player.x += 1
     
-    move_to(c, (10 * player_x, 10 * player_y))
+    move_to(c, (10 * player.x, 10 * player.y))
+
+
 def check_collisions():
-    global finished
-    if player_x == bot_x and player_y == bot_y:
+    global finished, player
+    if player.x == bot.x and player.y == bot.y:
         Text("You've Been Caught", (320, 240), size=40)
         sleep(2)
         finished = True
+
 def safe_player():
     place_player()
-while player_x == bot_x and player_y == bot_y:
-    place_player()
+    while player.x == bot.x and player.y == bot.y:
+        print('safety')
+        place_player()
+
 
 begin_graphics()
 finished = False
 
-safe_player()
 place_robot()
+safe_player()
+
 while not finished:
     move_player()
     move_robot()
     check_collisions()
+
 end_graphics()
