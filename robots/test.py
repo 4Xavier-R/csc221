02f3fa9b010cbf1003ab1158a3b60a4f8d7fe800
Xavier Rodriguez <xr63645@email.vccs.edu>
2023-07-19 11:52:37 -0400

    box

--- robots/test.py
+++ robots/test.py
@@ -1,9 +1,4 @@
-from gasp import *
+from gasp import * 
 begin_graphics()
-key_text = Text("a" , (320, 240), size = 48)
-while True:
-    key = update_when('key_pressed')
-    remove_from_screen(key_text)
-    key_text = Text(key, (320, 240), size = 48)
-    if key == 'q':
-        break
+Box((10, 10), 10, 10, filled = True, color=color.RED)
+update_when('key_pressed')
